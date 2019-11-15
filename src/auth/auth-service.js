b8d3e3fb3f0640aa0eda9ccd0737faeb51aef176
Eric Wang <ericwang27759@gmail.com>
2019-11-14 19:25:19 -0500

    secure login

--- src/auth/auth-service.js
+++ src/auth/auth-service.js
@@ -1,3 +1,7 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('../config')
+
 const AuthService = {
     parseBasicToken(token) {
         return Buffer
@@ -9,6 +13,20 @@ const AuthService = {
         return db('thingful_users')
             .where({user_name})
             .first()
+    },
+    comparePasswords(password, hash) {
+        return bcrypt.compare(password, hash)
+    },
+    createJwt(subject, payload) {
+        return jwt.sign(payload, config.JWT_SECRET, {
+            subject,
+            algorithm: 'HS256'
+        })
+    },
+    verifyJwt(token) {
+        return jwt.verify(token, config.JWT_SECRET, {
+            algorithms: ['HS256']
+        })
     }
 }
 
